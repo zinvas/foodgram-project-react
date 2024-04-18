93e5e1af2f3c1363d1d4fd1d10da4f2364b85319
Vasily Zinovyev <zinavasa@gmail.com>
2024-04-18 19:36:33 +0300

    base64 image change

--- backend/food_api/serializers.py
+++ backend/food_api/serializers.py
@@ -5,6 +5,7 @@ from djoser.serializers import (
     UserCreateSerializer as DjoserUserCreateSerializer,
     UserSerializer as DjoserUserSerializer
 )
+from drf_extra_fields.fields import Base64ImageField
 from rest_framework.fields import (
     ImageField,
     IntegerField,
@@ -157,17 +158,6 @@ class IngredientsRecipesSerializer(ModelSerializer):
             'measurement_unit',
             'amount'
         ]
-
-
-class Base64ImageField(ImageField):
-    def to_internal_value(self, data):
-        if isinstance(data, str) and data.startswith('data:image'):
-            format, imgstr = data.split(';base64,')
-            ext = format.split('/')[-1]
-
-            data = ContentFile(base64.b64decode(imgstr), name='temp.' + ext)
-
-        return super().to_internal_value(data)
 
 
 class RecipeSerializer(ModelSerializer):
